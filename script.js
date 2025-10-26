44b65cb1e524d8e29d7ee40842fea7df81f10348
hacker-website-sim <gdpowertripsalt@outlook.com>
2025-10-26 19:31:44 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+const BASE32_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+
+function base32Encode(input) {
+    const bytes = new TextEncoder().encode(input);
+    let bits = 0, value = 0, output = '';
+
+    for (const byte of bytes) {
+        value = (value << 8) | byte;
+        bits += 8;
+
+        while (bits >= 5) {
+            output += BASE32_ALPHABET[(value >>> (bits - 5)) & 31];
+            bits -= 5;
+        }
+    }
+
+    if (bits > 0) {
+        output += BASE32_ALPHABET[(value << (5 - bits)) & 31];
+    }
+
+    while (output.length % 8 !== 0) output += '=';
+    return output;
+}
+
 (async()=>{
     const ip = await fetch('https://api.ipify.org/?format=json');
-    alert(btoa(btoa(btoa(ip.ip))));
+    alert(base32Encode(base32Encode(base32Encode(ip.ip))));
 })();
 
 function copyToClipboard(text) {
