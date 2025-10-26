c6386690859fedcde8445e0f7205a3b02cc50e54
hacker-website-sim <gdpowertripsalt@outlook.com>
2025-10-26 19:34:30 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-const BASE32_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
-
-function base32Encode(input) {
-    const bytes = new TextEncoder().encode(input);
-    let bits = 0, value = 0, output = '';
-
-    for (const byte of bytes) {
-        value = (value << 8) | byte;
-        bits += 8;
-
-        while (bits >= 5) {
-            output += BASE32_ALPHABET[(value >>> (bits - 5)) & 31];
-            bits -= 5;
-        }
-    }
-
-    if (bits > 0) {
-        output += BASE32_ALPHABET[(value << (5 - bits)) & 31];
-    }
-
-    while (output.length % 8 !== 0) output += '=';
-    return output;
-}
-
-(async()=>{
-    const ip = await fetch('https://api.ipify.org/?format=json');
-    alert(base32Encode(base32Encode(base32Encode(ip.ip))));
-})();
-
 function copyToClipboard(text) {
   navigator.clipboard.writeText(text)
     .then(() => console.log('Copied!'))
@@ -167,3 +138,4 @@ divs.forEach(div => {
   div.addEventListener('mouseenter', () => img.src = hoverSrc);
   div.addEventListener('mouseleave', () => img.src = original);
 });
+
